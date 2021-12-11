a9042e98d1aff1cc58a05a5d71bd59c806290cba
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2021-12-11 12:54:21 +0200

    aoc2021 add some timings per day

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,82 @@
 const std = @import("std");
 
-const day1 = @import("day1.zig").day1;
-const day2 = @import("day2.zig").day2;
-const day3 = @import("day3.zig").day3;
-const day4 = @import("day4.zig").day4;
-const day5 = @import("day5.zig").day5;
-const day6 = @import("day6.zig").day6;
-const day7 = @import("day7.zig").day7;
-const day8 = @import("day8.zig").day8;
-const day9 = @import("day9.zig").day9;
-const day10 = @import("day10.zig").day10;
-const day11 = @import("day11.zig").day11;
+const print = std.debug.print;
 
 pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
-    var allocator = &arena.allocator;
+    const startDur:i128 = std.time.nanoTimestamp();
 
-    try day1(allocator);
-    try day2(allocator);
-    try day3(allocator, "../input_day3.txt");
-    try day4(allocator, "../input_day4.txt");
-    try day5(allocator, "../input_day5.txt");
-    try day6(allocator, "../input_day6.txt");
-    try day7(allocator, @embedFile("../input_day7.txt"));
-    try day8(allocator, @embedFile("../input_day8.txt"));
-    try day9(allocator, @embedFile("../input_day9.txt"));
-    try day10(allocator, @embedFile("../input_day10.txt"));
-    try day11(allocator, @embedFile("../input_day11.txt"));
+    var allocator = &arena.allocator;
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day1.zig").day1(allocator);
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day1 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day2.zig").day2(allocator);
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day2 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day3.zig").day3(allocator, "../input_day3.txt");
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day3 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day4.zig").day4(allocator, "../input_day4.txt");
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day4 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day5.zig").day5(allocator, "../input_day5.txt");
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day5 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day6.zig").day6(allocator, "../input_day6.txt");
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day6 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day7.zig").day7(allocator, @embedFile("../input_day7.txt"));
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day7 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day8.zig").day8(allocator, @embedFile("../input_day8.txt"));
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day8 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day9.zig").day9(allocator, @embedFile("../input_day9.txt"));
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day9 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day10.zig").day10(allocator, @embedFile("../input_day10.txt"));
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day10 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+    {
+        const t:i128 = std.time.nanoTimestamp();
+        try @import("day11.zig").day11(allocator, @embedFile("../input_day11.txt"));
+        const t1:i128 = std.time.nanoTimestamp();
+        print("Day11 dur: {}us\n\n", .{@divTrunc(t1 - t, @as(i128, 1000))});
+    }
+
+    const endDur:i128 = std.time.nanoTimestamp();
+    print("Total dur: {}us\n\n", .{@divTrunc(endDur - startDur, @as(i128, 1000))});
 
 }
