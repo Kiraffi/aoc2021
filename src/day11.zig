b30b72d04efa5660a6c2dd419fc5f97b727277e1
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2021-12-14 21:31:33 +0200

    aoc2021 make day11 more simd-like code

--- src/day11.zig
+++ src/day11.zig
@@ -6,17 +6,23 @@ const print = std.debug.print;
 
 pub fn day11(_: *std.mem.Allocator, inputFile: []const u8, printBuffer: []u8) anyerror! usize
 {
-    var board: [12*12]u8 = std.mem.zeroes([12*12]u8);
+    // (1 + 1 << 6) + (1 << 12) + (1 << 18) + (1 << 24)
+    //+ (1 << 30 )+ (1 << 36 ) + (1 << 42) + (1 << 48) + (1 << 54)
+    // = 18300341342965825 = 0x41041041041041
+    const firstBitAll: u64 = 0x41041041041041;
+
+    var board = std.mem.zeroes([10]u64);
+    var flashTable = std.mem.zeroes([10]u64);
     {
         var lines = std.mem.tokenize(u8, inputFile, "\r\n");
-        var y:u32 = 1;
+        var y:u32 = 0;
         while (lines.next()) |line|
         {
             var x: u32 = 0;
             while(x < line.len) : (x += 1)
             {
-                const index:u32 = x + 1 + y * 12;
-                board[index] = line[x] - '0';
+                const index:u32 = 6 * x;
+                board[y] |= @intCast(u64, (line[x] - '0') + 6) << @intCast(u6, index);
             }
             y += 1;
         }
@@ -28,32 +34,71 @@ pub fn day11(_: *std.mem.Allocator, inputFile: []const u8, printBuffer: []u8) an
         var flashesAt100: u32 = 0;
         while(steps < 100 or allFlashStep == 0) : (steps += 1)
         {
-            var y:u32 = 1;
-            while(y < 11) : (y += 1)
+            var y:u32 = 0;
+            while(y < 10) : (y += 1)
             {
-                var x: u32 = 1;
-                while(x < 11) : (x += 1)
-                {
-                    addValue(&board, x, y);
-                }
+                board[y] += firstBitAll;
             }
-            var flashCountPerStep: u32 = 0;
-            y = 13;
-            while(y < 12 * 12) : (y += 1)
+            var newFlashes: u64 = 1;
+            while(newFlashes != 0)
             {
-                if(board[y] > 9)
+                newFlashes = 0;
+
+                // 5th bit set.
+                const flashMask: u64 = firstBitAll << 4;
+
+                var prevFlashRow: u64 = 0;
+                var currFlashRow = (board[0] & flashMask) & (~flashTable[0]);
+                var nextFlashRow = (board[1] & flashMask) & (~flashTable[1]);
+
+                y = 0;
+                while(y < 9) : (y += 1)
                 {
-                    flashCountPerStep += 1;
-                    board[y] = 0;
+                    nextFlashRow = (board[y + 1] & flashMask) & (~flashTable[y + 1]);
+                    const scrollingFlashRow = (prevFlashRow + currFlashRow + nextFlashRow) >> 4;
+
+                    newFlashes |= scrollingFlashRow;
+
+                    var addValue: u64 = scrollingFlashRow;
+                    addValue += (scrollingFlashRow << 6);
+                    addValue += (scrollingFlashRow >> 6);
+                    _ = @addWithOverflow(u64, board[y], addValue, &board[y]);
+
+                    flashTable[y] |= currFlashRow;
+                    prevFlashRow = currFlashRow;
+                    currFlashRow = nextFlashRow;
                 }
+
+                const scrollingFlashRow = (prevFlashRow + currFlashRow) >> 4;
+
+                var addValue: u64 = scrollingFlashRow;
+                addValue += (scrollingFlashRow << 6);
+                addValue += (scrollingFlashRow >> 6);
+                _ = @addWithOverflow(u64, board[9], addValue, &board[9]);
+
+                flashTable[9] |= currFlashRow;
+
+
+                newFlashes |= scrollingFlashRow;
             }
+
             y = 0;
-            while(y < 12) : (y += 1)
+            var flashCountPerStep: u32 = 0;
+            while(y < 10) : (y += 1)
             {
-                board[y] = 0;
-                board[y + 11 * 12] = 0;
-                board[y * 12] = 0;
-                board[11 + y * 12] = 0;
+                flashCountPerStep += @popCount(u64, flashTable[y]);
+
+                // reset exploded ones to 6
+                // andMask ~6 for exploded
+                const andMask = flashTable[y] | flashTable[y] >> 1 | flashTable[y] >> 4 | flashTable[y] << 1;
+                const orMask = flashTable[y] >> 2 | flashTable[y] >> 3;
+
+                board[y] &= ~andMask;
+                board[y] |= orMask;
+
+                // clear 5 upper bits for sure
+                board[y] &= @as(u64, 0x07ff_ffff_ffff_ffff);
+                flashTable[y] = 0;
             }
 
             if(flashCountPerStep == 100 and allFlashStep == 0)
@@ -70,28 +115,3 @@ pub fn day11(_: *std.mem.Allocator, inputFile: []const u8, printBuffer: []u8) an
         return res.len + res2.len;
     }
 }
-
-
-fn addValue(board: []u8, x: u32, y: u32) void
-{
-    const index:u32 = x + y * 12;
-    board[index] += 1;
-    if(board[index] == 10)
-    {
-        const x2: u32 = x + 1;
-        const y2: u32 = y + 1;
-
-        var y1: u32 = y - 1;
-        while(y1 <= y2) : (y1 += 1)
-        {
-            var x1: u32 = x - 1;
-            while(x1 <= x2) : (x1 += 1)
-            {
-                if(x1 == x and y1 == y)
-                    continue;
-                addValue(board, x1, y1);
-            }
-        }
-    }
-}
-
